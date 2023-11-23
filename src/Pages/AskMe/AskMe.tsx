a7aecea45bc2a215d720f2ae8c23fef0b82fb428
rajul786dixit <rajul786dixit@gmail.com>
2023-11-23 17:32:34 +0530

    client and server setup done

--- src/Pages/AskMe/AskMe.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AskMe = () => {
-  return (
-    <div>AskMe</div>
-  )
-}
-
-export default AskMe
--- src/Pages/Home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './Home'
-
-describe('Home Page', () => {
-    test('', () => {
-        render(<Home />)
-    })
-});
